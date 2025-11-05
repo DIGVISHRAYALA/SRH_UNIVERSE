3f4ce9ecb0647ce6f098d18d074f68da9bba6e4f
DIGVISHRAYALA <152776370+DIGVISHRAYALA@users.noreply.github.com>
2025-11-05 20:56:53 +0530

    Add team1 and team2 fields to Room schema

--- backend/models/Room.js
+++ backend/models/Room.js
@@ -1,8 +1,28 @@
-// models/Room.js
+// // models/Room.js
+// const mongoose = require("mongoose");
+// const roomSchema = new mongoose.Schema({
+//   title: { type: String, required: true },
+//   createdAt: { type: Date, default: Date.now },
+// });
+
+// module.exports = mongoose.model("Room", roomSchema);
+
+
+
+
+
+
 const mongoose = require("mongoose");
+
 const roomSchema = new mongoose.Schema({
   title: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now },
+  team1: { type: String, required: true },
+  team2: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model("Room", roomSchema);
+
+
+
+
