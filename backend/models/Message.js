881e44e10ba152d79adb4993c22a221882c395dc
DIGVISHRAYALA <152776370+DIGVISHRAYALA@users.noreply.github.com>
2025-11-05 20:56:23 +0530

    Uncomment Message model schema
    Uncommented the Message model schema definition.

--- backend/models/Message.js
+++ backend/models/Message.js
@@ -1,3 +1,19 @@
+// // models/Message.js
+// const mongoose = require("mongoose");
+
+// const messageSchema = new mongoose.Schema({
+//   roomId: { type: String, required: true },
+//   user: { type: String, required: true },
+//   text: { type: String, required: true },
+//   time: { type: Date, default: Date.now }
+// });
+
+// module.exports = mongoose.model("Message", messageSchema);
+
+
+
+
+
 // models/Message.js
 const mongoose = require("mongoose");
 
